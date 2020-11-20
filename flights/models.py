c59449d798f2558d9bfccc6ca30e7847c23655c6
SD <sashaluckyman@gmail.com>
2020-11-19 22:45:58 -0800

    create migration model and apply migration

--- flights/models.py
+++ flights/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+# After models are created, we create migration by executing:
+#  python manage.py makemigrations
+#  and to apply migration - python manage.py migrate
+
+class Flight(models.Model):
+    origin = models.CharField(max_length=64)
+    destination = models.CharField(max_length=64)
+    duration = models.IntegerField()
