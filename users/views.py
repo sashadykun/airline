57d001839e8f04170d6b8dc8af2277f53f8e75ed
SD <sashaluckyman@gmail.com>
2020-12-23 20:46:39 -0800

    add user and login options

--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 
@@ -8,9 +9,23 @@ from django.urls import reverse
 def index(request):
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse("login"))
+    return render(request, "users/user.html")
 
 
 def login_view(request):
+    if request.method == 'POST':
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "users/login.html", {
+                "message": "Invalid Credentials."
+            })
+
+
     return render(request, "users/login.html")
 
 def logout_view(request):
